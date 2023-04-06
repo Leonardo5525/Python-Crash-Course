431f314f74f61d8de50327cc0996871f5addc485
leonardo <leonardonaka75@gmail.com>
2023-04-05 22:44:51 -0300

    Pag at 40

--- Part I/Chapter3 - List.py
+++ Part I/Chapter3 - List.py
@@ -2,3 +2,22 @@
 bicycles = ['trek', 'cannondale', 'redline', 'specialized']
 print(bicycles[0])
 
+#Returning last value on the list or -2 (before the last) -3 (befora -2) and continue
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles[-1])
+
+#Try ir Yourself - 3-1. Names: 
+names = ['leo', 'vitoria', 'joao', 'lucas']
+print(names)
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+
+#Try it Yourself - 3-2. Greetings:
+names = ['leo naka', 'vitoria', 'joao', 'lucas']
+message = (names[0].capitalize())
+print(message)
+
+#Try it Yourself - 3-3. Your Own List: 
+
