ec7db387a01faace596777365b5fd354931431e7
Leonardo-PC <leonardonaka75@gmail.com>
2023-06-21 10:13:33 -0300

    add

--- Part I/Chapters - Testes.py
+++ Part I/Chapters - Testes.py
@@ -1,9 +1,19 @@
-def build_car(many, model, **info):
-    profile = {}
-    profile['Manifacture'] = many
-    profile['Model'] = model
-    for key, value in info.items():
-        profile[key] = value
-    return profile
+class User:
+    def __init__(self, first_name, last_name, age, city):
+        self.first_name =  first_name
+        self.last_name = last_name
+        self.age = age
+        self.city = city
+    
+    def describe_user (self):
+        print(f'The user name is {self.first_name} {self.last_name}')
+        print(f'The user is {self.age} years old and lives in {self.city}')
+    
+    def greet_user(self):
+        print(f'Hello {self.first_name}{self.last_name}')
+    
+user1 = User('leonardo', 'nakamura', 26, 'marilia')
+user1.describe_user()
+user1.greet_user()
 
-print(build_car('fiat', 'GM', location='Marília',color='blue'))
+
