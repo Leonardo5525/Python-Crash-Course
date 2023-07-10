8a3ec3d2a838366715b1ac6fb363585a9f90c2b9
Leonardo-PC <leonardonaka75@gmail.com>
2023-07-10 08:02:43 -0300

    terminar class

--- Part I/Chapter 9 - Class.py
+++ Part I/Chapter 9 - Class.py
@@ -60,17 +60,25 @@ restaurant.open_restaurant()
 9-3. Users: Make a class called User. Create two attributes called first_name and last_name, and then create several other attributes that are typically stored in a user profile. Make a method called describe_user() that prints a summary of the user’s information. Make another method called greet_user() that prints a personalized greeting to the user. Create several instances representing different users, and call both methods
 for each user.
 '''
+
 class User:
     def __init__(self, first_name, last_name, age, city):
         self.first_name =  first_name
         self.last_name = last_name
         self.age = age
         self.city = city
-    
+
     def describe_user (self):
         print(f"The user's name is {self.first_name} {self.last_name}")
-        print(f'The user is {self.age} years old and lives in {self.lacation}')
-        
+        print(f'The user is {self.age} years old and lives in {self.city}')
+    
+    def greet_user(self):
+        print (f'Hello {self.first_name}')
+
+usuario = User('leo', 'nakamura', 25, 'marilia' )
+usuario.describe_user()
+usuario.greet_user()
+
 '''
 Try It Yourself 9-4. Number Served: Start with your program from Exercise 9-1 (page 166). Add an attribute called number_served with a default value of 0. Create an instance called restaurant from this class. Print the number of customers the restaurant has served, and then change this value and print it again. 
 Add a method called set_number_served() that lets you set the number of customers that have been served. Call this method with a new number and print the value again. Add a method called increment_number_served() that lets you increment the number of customers who’ve been served. Call this method with any number you like that could represent how many customers were served in, say, a day of business. 
@@ -109,15 +117,96 @@ restaurant.restaurant()
 9-5. Login Attempts: Add an attribute called login_attempts to your User class from Exercise 9-3 (page 166). Write a method called increment_ login_attempts() that increments the value of login_attempts by 1. Write another method called reset_login_attempts() that resets the value of login_ attempts to 0. Make an instance of the User class and call increment_login_attempts() several times. Print the value of login_attempts to make sure it was incremented properly, and then call reset_login_attempts(). Print login_attempts again to make sure it was reset to 0.
 '''
 
+class User:
+    def __init__(self, first_name, last_name, age, city):
+        self.first_name =  first_name
+        self.last_name = last_name
+        self.age = age
+        self.city = city
+        self.login_attempts = 0
+
+    def describe_user (self):
+        print(f"The user's name is {self.first_name} {self.last_name}")
+        print(f'The user is {self.age} years old and lives in {self.city}')
+    
+    def greet_user(self):
+        print (f'Hello {self.first_name}')
+
+    def increment_login_attempts(self):
+        self.login_attempts += 1
+
+usuario = User('leo', 'nakamura', 25, 'marilia' )
+usuario.describe_user()
+usuario.greet_user()
+usuario.increment_login_attempts()
+usuario.increment_login_attempts()
+print(f'Login attempts = {usuario.login_attempts}')
 
 '''
 Try It Yourself 9-6. Ice Cream Stand: An ice cream stand is a specific kind of restaurant. Write a class called IceCreamStand that inherits from the Restaurant class you wrote in Exercise 9-1 (page 166) or Exercise 9-4 (page 171). Either version of the class will work; just pick the one you like better. Add an attribute called flavors that stores a list of ice cream flavors. Write a method that displays these flavors. Create an instance of IceCreamStand, and call this method. 
 '''
 
+class Restaurant:
+    '''A simple restaurante class'''
+
+    def __init__(self, restaurant_name, cusine_type):
+        self.restaurante_name =  restaurant_name
+        self.cusine_type = cusine_type
+
+    def describe_restaurant(self):
+        '''Describes a restaurant'''
+        print(f'The restaurant is called {self.restaurante_name}')
+        print(f'The type is {self.cusine_type}')
+    
+    def open_restaurant(self):
+        '''Describes that restaurantis open'''
+        print(f'The restaurant {self.restaurante_name}is open')
+    
+class IceCreamStand(Restaurant):
+    def __init__(self, restaurant_name, cusine_type,flavors):
+        super().__init__(restaurant_name,cusine_type)
+        self.flavors = flavors
+
+    def display_flavors(self):
+        # Display ice cream flavors
+        print(f'The flavors are: ')
+        for flavor in self.flavors:
+            print(f'-{flavor}')
+
+consumidor = IceCreamStand('Lunata', 'sorvete', ['choco', 'baunilha', 'menta'])
+consumidor.display_flavors()
 
 '''
 9-7. Admin: An administrator is a special kind of user. Write a class called Admin that inherits from the User class you wrote in Exercise 9-3 (page 166) or Exercise 9-5 (page 171). Add an attribute, privileges, that stores a list of strings like "can add post", "can delete post", "can ban user", and so on. Write a method called show_privileges() that lists the administrator’s set of privileges. Create an instance of Admin, and call your method. 
 '''
+
+class User:
+    def __init__(self, first_name, last_name, age, city):
+        self.first_name =  first_name
+        self.last_name = last_name
+        self.age = age
+        self.city = city
+
+    def describe_user (self):
+        print(f"The user's name is {self.first_name} {self.last_name}")
+        print(f'The user is {self.age} years old and lives in {self.city}')
+    
+    def greet_user(self):
+        print (f'Hello {self.first_name}')
+
+class Admin(User):
+    def __init__(self, first_name, last_name, age, city, privileges):
+        super().__init__(first_name, last_name, age, city)
+        self.privileges = privileges
+    
+    def show_privileges(self):
+        print(f'Privileges: ')
+        for privilege in self.privileges:
+            print(f'- {privilege}')
+
+user =  Admin('leo', 'Admin', 'marilia', 26, ['can add post', 'can delete post', 'can ban user'])
+user.describe_user()
+user.show_privileges()
 
 '''
 9-8. Privileges: Write a separate Privileges class. The class should have one attribute, privileges, that stores a list of strings as described in Exercise 9-7. Move the show_privileges() method to this class. Make a Privileges instance as an attribute in the Admin class. Create a new instance of Admin and use your method to show its privileges.
--- Part I/teste2.py
+++ Part I/teste2.py
@@ -4,8 +4,28 @@ class User:
         self.last_name = last_name
         self.age = age
         self.city = city
-        self.inc
-    
+
     def describe_user (self):
         print(f"The user's name is {self.first_name} {self.last_name}")
-        print(f'The user is {self.age} years old and lives in {self.lacation}')
+        print(f'The user is {self.age} years old and lives in {self.city}')
+    
+    def greet_user(self):
+        print (f'Hello {self.first_name}')
+
+class Admin(User):
+    def __init__(self, first_name, last_name, age, city, privileges):
+        super().__init__(first_name, last_name, age, city)
+        self.privileges = privileges
+    
+    def show_privileges(self):
+        print(f'Privileges: ')
+        for privilege in self.privileges:
+            print(f'- {privilege}')
+
+user =  Admin('leo', 'Admin', 'marilia', 26, ['can add post', 'can delete post', 'can ban user'])
+user.describe_user()
+user.show_privileges()
+    
+
+
+usuario = User('leo', 'nakamura', 25, 'marilia' )
